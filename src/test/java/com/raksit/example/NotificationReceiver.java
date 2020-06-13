b8df660d40b0cfb86398bfeaa5ec9eaf1d5e3474
Raksit <raksit.m@ku.th>
2020-06-13 20:34:48 +0700

    ✅️ create a NotificationReceiver powered by Kafka polling

---
+++ src/test/java/com/raksit/example/NotificationReceiver.java
@@ -0,0 +1,58 @@
+package com.raksit.example;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public class NotificationReceiver {
+
+  private static final int MAX_ALLOWED_LATENCY = 5000;
+
+  private final String topic;
+
+  public NotificationReceiver(String topic) {
+    this.topic = topic;
+  }
+
+  public List<String> poll() throws IOException {
+
+    List<String> records = new ArrayList<>();
+
+    KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(getProperties());
+
+    kafkaConsumer.subscribe(Collections.singletonList(topic));
+
+    try {
+      long endPollingTimestamp = System.currentTimeMillis() + MAX_ALLOWED_LATENCY;
+
+      while ( System.currentTimeMillis() < endPollingTimestamp ) {
+        ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.of(1, ChronoUnit.SECONDS));
+        for ( ConsumerRecord<String, String> next : consumerRecords ) {
+          System.out.println(next.value());
+          records.add(next.value());
+        }
+      }
+    } finally {
+      kafkaConsumer.close();
+    }
+    return records;
+  }
+
+  private Properties getProperties() throws IOException {
+    FileReader reader = new FileReader(FileUtils.getFile("src", "test", "resources", "kafka.properties"));
+    Properties properties = new Properties();
+    properties.load(reader);
+
+    return properties;
+  }
+}
